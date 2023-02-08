967d56faea0368416244331787dfcb35c9959fc1
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-02-08 22:32:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://du-link.in/api'
+    url = f'https://shareus.io/pub//api'
     params = {'api': SHORTNER_API,
               'url': link,
               }
