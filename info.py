300063443aa84d8856e01f4836228aab799ba335
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-02-08 23:02:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -65,7 +65,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 
 
-AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 12600))
+AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 300))
 AUTO_DELETE = environ.get('AUTO_DELETE', True)
 if AUTO_DELETE == "True":
     AUTO_DELETE = True
